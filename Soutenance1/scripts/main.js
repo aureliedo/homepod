02749b8080ff60c529e3db4320edca437033fccf
Aurélie <aureliedo@icloud.com>
2019-02-21 20:15:11 +0100

    page3

---
+++ Soutenance1/scripts/main.js
@@ -0,0 +1,60 @@
+/*const pictures 		  = document.querySelectorAll(".carrousel img")
+const sliderContainer = document.querySelector(".carrousel")
+const previousButton  = document.querySelector(".previous")
+const nextButton 	  = document.querySelector(".next")
+const dots			  = document.querySelectorAll("ul.dots li")
+
+let time = 3000,
+	step = -1366,
+	currentPicture = 0,
+	slide
+
+function slideInterval(){
+	slide = setInterval(() =>{
+		currentPicture = (currentPicture + 1) % pictures.length
+		display(currentPicture)
+	}, time)
+}
+
+slideInterval()
+
+sliderContainer.addEventListener("mouseover", () => {
+	clearInterval(slide)
+})
+sliderContainer.addEventListener("mouseleave", () => {
+	slideInterval()
+})
+
+previousButton.addEventListener("click", (e) => {
+	e.preventDefault()
+	currentPicture--
+	if(currentPicture < 0){
+		currentPicture = pictures.length - 1
+	}
+	display(currentPicture)
+})
+
+nextButton.addEventListener("click", (e) => {
+	e.preventDefault()
+	currentPicture++
+	if(currentPicture >= pictures.length){
+		currentPicture = 0
+	}
+	display(currentPicture)
+})
+
+function display (currentPos){
+	sliderContainer.style.left = currentPos * step + "px"
+  document.querySelector('a.current').classList.remove('current')
+	dots[currentPos].querySelector('a').classList.add('current')
+}
+for(let i = 0; i < dots.length; i++){
+	dots[i].querySelector("a").addEventListener("click", (e) => {
+		e.preventDefault()
+		this.setAttribute('data-test','test')
+		currentPicture = i
+		display(currentPicture)
+	})
+
+}
+*/
